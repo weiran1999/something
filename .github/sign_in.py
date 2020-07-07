2cadab7fd0584dd86c0dc0f5938168ae9f359dd1
weiran1999 <60456779+weiran1999@users.noreply.github.com>
2020-07-07 16:56:58 +0800

    Update sign_in.py

--- .github/sign_in.py
+++ .github/sign_in.py
@@ -5,9 +5,9 @@ def sign_in():
     browser = webdriver.Chrome()
     url = r"https://wxyqfk.zhxy.net/?yxdm=10623#/login"  
     browser.get(url)
-    browser.find_element_by_css_selector('input[type][placeholder="请输入学号"].van-field__control').send_keys("3120180802211")
-    browser.find_element_by_css_selector('input[type][placeholder="请输入姓名"].van-field__control').send_keys("孙伟然")
-    browser.find_element_by_css_selector('input[type][placeholder="请输入密码(初始密码为学号)"].van-field__control').send_keys("sun8100512")
+    browser.find_element_by_css_selector('input[type][placeholder="请输入学号"].van-field__control').send_keys("XUEHAO")
+    browser.find_element_by_css_selector('input[type][placeholder="请输入姓名"].van-field__control').send_keys("XINGMING")
+    browser.find_element_by_css_selector('input[type][placeholder="请输入密码(初始密码为学号)"].van-field__control').send_keys("MIMA")
     #browser.find_element_by_css_selector('button.sign-in-btn.van-button.van-button--info.van-button--normal').click()
     browser.find_element_by_xpath('//*[@id="app"]/div/div[2]/button').click()
     time.sleep(1)#有时候定位不到，就暂停一秒
