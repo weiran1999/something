fdd84d2ba726b9aa66a2ed7f421df4fdb0b84bba
weiran1999 <60456779+weiran1999@users.noreply.github.com>
2020-07-09 09:29:04 +0800

    Update sign_in.py

--- sign_in.py
+++ sign_in.py
@@ -19,7 +19,7 @@ def sign_in():
         browser.find_element_by_xpath('/html/body/div[4]/div[3]').click()
     except:
         pass
-    time.sleep(5)
+    time.sleep(15)
     browser.find_element_by_xpath('/html/body/div[1]/div/div[3]/ul/li[1]').click()
     time.sleep(15)#必要的等待加载
     browser.find_element_by_xpath('//*[@id="app"]/div/div[3]/button').click()
